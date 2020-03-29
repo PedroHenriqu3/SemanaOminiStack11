5a53ff1e175b87597f805bad48c1faca17c4ab5f
Pedro Henrique <userpedrohenrique0@gmail.com>
2020-03-29 00:31:28 -0300

    Back End Application

--- backend/src/Controllers/OngController.js
+++ backend/src/Controllers/OngController.js
@@ -2,10 +2,10 @@ const connection = require('../database/connection');
 const crypto = require('crypto');
 
 module.exports = {
- 
+
  async index(request, response) {
   const ongs = await connection('ongs').select('*');
- 
+
   return response.json(ongs);
  },
 
